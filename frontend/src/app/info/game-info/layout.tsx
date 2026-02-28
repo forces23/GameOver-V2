ffa863b86bde77ce5becd95072118815f7e999be
Bobby Lawson <bobby.lawson17@gmail.com>
2026-02-28 01:12:26 -0600

    adding Suspense to game-info layout

--- frontend/src/app/info/game-info/layout.tsx
+++ frontend/src/app/info/game-info/layout.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import PageSkeleton from '@/components/PageSkeleton'
+import React, { Suspense } from 'react'
 
 export default function layout({
     children
@@ -7,8 +8,10 @@ export default function layout({
     children: React.ReactNode
 }>) {
     return (
-        <div className="flex grow w-full">
-            {children}
-        </div>
+        <Suspense fallback={<PageSkeleton />}>
+            <div className="flex grow w-full">
+                {children}
+            </div>
+        </Suspense>
     )
 }
